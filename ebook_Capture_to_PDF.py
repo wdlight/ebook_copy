6a6850c695f72ba7c1ac8bf4f32118df04b785e9
wdlight <wdlight@naver.com>
2022-12-16 19:37:49 +0900

    fix scan size

--- ebook_Capture_to_PDF.py
+++ ebook_Capture_to_PDF.py
@@ -7,8 +7,14 @@ import winsound
 btn_next = pyautogui.locateOnScreen("next_button.png", grayscale=True)
 print (f" next button location : [{btn_next}]")
 
+while btn_next is None:
+    btn_next = pyautogui.locateOnScreen("next_button.png", grayscale=True)
+    if btn_next is None:
+        pyautogui.prompt(" Check out the next button and Retry.")
+    
+
 ## File name 지정
-book_name = pyautogui.prompt("ebook 파일명을 지정하세요.", "ebook")
+book_name = pyautogui.prompt("ebook 파일명을 지정하고 \n각 scan 영역 4개 point를 Click하세요!!.", "ebook")
 print( f"ebook filename: [{book_name}]" )
 
 
@@ -17,6 +23,7 @@ print( f"ebook filename: [{book_name}]" )
 from pynput.mouse import Listener
 
 coord = []
+
 def on_click(x,y, button, pressed):
         if pressed:
             x = int(x)
@@ -24,7 +31,7 @@ def on_click(x,y, button, pressed):
             coord.append((x,y)) # You can use a list also here.
             print( f" each click - input coord: {coord}")   
             if len(coord) == 4:
-                print( f"input coord: {coord}")                   
+                print( f"input coord: {coord}")  
                 listener.stop()  
                 
             return
@@ -34,26 +41,23 @@ with Listener( #on_move=on_move, on_scroll=on_scroll,
 
 ## 화면 영역 설정 
 # 3개의 포인터 중 가장 큰 값으로 영역 잡기.
-min_x = 500
-min_y = 500
-max_x = 500
-max_y = 500
+import numpy as np
 
-for x,y in coord :
-    print (f"{x},{y}")
-    min_x =  x if x < min_x else  min_x
-    min_y =  y if y < min_y else  min_y
-    max_x =  x if x > max_x else  max_x
-    max_y =  y if y > max_y else  max_y
+min_x = np.min( coord, axis=0)[0]
+min_y = np.min( coord, axis=0)[1]
+max_x = np.max( coord, axis=0)[0]
+max_y = np.max( coord, axis=0)[1]
 
 topleft = ( min_x, min_y )
 bottomright = ( max_x, max_y )
+
+print( f"scan position : {topleft},{bottomright}")
 
 from PIL import Image
 
 def copyImage(page):    
     book_page = f"{book_name}_{page}.png"    
-    pyautogui.screenshot( book_page, region = (min_x,min_y, max_x, max_y ))
+    pyautogui.screenshot( book_page, region = (min_x,min_y, max_x - min_x, max_y - min_y ))
 
 ###########################################
 ## 마지막 페이지  받아오기
@@ -63,7 +67,12 @@ print (f"pages: {pages}")
 
 ###########################################
 ## Scan each images..
-for i in range( 0, int(pages) ):
+import os
+out_dir = book_name
+os.mkdir (out_dir)
+os.chdir(out_dir)
+
+for i in range( 0, int(pages) + 1):
     ##
     print (f"current page{i}")    
     copyImage(i)
@@ -79,7 +88,7 @@ img_main = img_1.convert('RGB')
 
 img_list = []
 for i in range( 1, int(pages) + 1 ):
-    img_name = f"HandsOnML/HandsOnML_SK_Keras_TF_{i}.png"
+    img_name = f"{book_name}_{i}.png"
     print ( f" file appending: {img_name}")
     img_1 = Image.open(img_name)
     img_rgb_1 = img_1.convert('RGB')
